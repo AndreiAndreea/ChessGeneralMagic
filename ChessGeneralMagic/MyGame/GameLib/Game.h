d1155e91e1aa1a464939ea76cfafd2e0ae1ba342
AndreeaAndrei <andreiandreea749@gmail.com>
2023-07-10 15:37:38 +0300

    edit the necessary projects

---
+++ ChessGeneralMagic/MyGame/GameLib/Game.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+
+class Game
+{
+
+public:
+	void Play();
+	void Addition(int x, int y);
+
+};
+
---
+++ ChessGeneralMagic/MyGame/GameTests/Test1.cpp
@@ -0,0 +1,5 @@
+ 
+void TEST(Addition, HandlesZeroInput)
+{
+	EXPECT_EQ(Adition(3,5), 8);
+}
