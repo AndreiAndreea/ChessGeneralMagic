d177f33e54b134abd4860bf86d7e9638b3b5d82f
AndreeaAndrei <andreiandreea749@gmail.com>
2023-07-12 15:45:37 +0300

    Implemented canMove method for Bishop and tests for it.

--- ChessGeneralMagic/MyGame/GameLib/Bishop.cpp
+++ ChessGeneralMagic/MyGame/GameLib/Bishop.cpp
@@ -1,7 +1,6 @@
 #include "Bishop.h"
 #include "Board.h"
 
-
 Bishop::Bishop(EPieceColor color) 
     : Piece (EPieceType::Bishop, color)
 {
@@ -10,5 +9,31 @@ Bishop::Bishop(EPieceColor color)
 
 bool Bishop::CanMove(Position startPos, Position endPos, const Board& board)
 {
-    return false;
+
+	if (std::abs(startPos.first - endPos.first) != std::abs(startPos.second - endPos.second))
+		return false;
+
+	BoardType localBoard = board.GetBoard();
+
+	int startRow = startPos.first;
+	int startCol = startPos.second;
+
+	int endRow = endPos.first;
+	int endCol = endPos.second;
+
+	int rowStep = (startRow < endRow) ? 1 : -1;
+	int colStep = (startCol < endCol) ? 1 : -1;
+
+	int currentRow = startRow + rowStep;
+	int currentCol = startCol + colStep;
+
+	while (currentRow != endRow && currentCol != endCol)
+	{
+		if (localBoard[currentRow][currentCol] != nullptr)
+			return false;
+
+		currentRow += rowStep;
+		currentCol += colStep;
+	}
+    return true;
 }
---
+++ ChessGeneralMagic/MyGame/GameTests/BishopTest.cpp
@@ -0,0 +1,53 @@
+#include "gtest/gtest.h"
+
+#include "Bishop.h"
+#include "Board.h"
+
+// Define a fixture for the Bishop tests
+class BishopTest : public ::testing::Test {
+
+	void SetUp() override
+	{	
+		bishop = new Bishop(EPieceColor::White);
+	}
+
+	void TearDown() override
+	{
+		delete bishop;
+	}
+
+protected:
+	Bishop* bishop;
+
+};
+
+TEST_F(BishopTest, ValidMove) {
+	Board board;
+	//Bishop bishop(EPieceColor::White);
+
+	Position startPos(2, 2);
+	Position endPos(5, 5);
+	bool canMove = bishop->CanMove(startPos, endPos, board);
+
+	EXPECT_TRUE(canMove);
+}
+
+TEST_F(BishopTest, ValidMove2) {
+	Board board;
+	//Bishop bishop(EPieceColor::White);
+
+	Position startPos(4, 3);
+	Position endPos(6, 5);
+	bool canMove = bishop->CanMove(startPos, endPos, board);
+
+	EXPECT_TRUE(canMove);
+}
+
+TEST_F(BishopTest, InvalidMove) {
+	Board board;
+	Position startPos(2, 2);
+	Position endPos(4, 5);
+	bool canMove = bishop->CanMove(startPos, endPos, board);
+
+	EXPECT_FALSE(canMove);
+}
